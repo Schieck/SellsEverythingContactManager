0b52ced115b528ba406dd779786e7ec221c4c1a2
Schieck <42654226+Schieck@users.noreply.github.com>
2019-01-19 10:13:46 -0200

    Architecture construction initial commit

---
+++ src/SEContactManager.UI.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SEContactManager.UI.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
